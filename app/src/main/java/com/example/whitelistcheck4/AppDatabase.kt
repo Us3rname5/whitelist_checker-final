7afe791789e5c63db212a24ee60f1f56c109333e
Us3rname5 <khalikovshamil1@gmail.com>
2026-07-23 12:58:42 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/example/whitelistcheck4/AppDatabase.kt
+++ app/src/main/java/com/example/whitelistcheck4/AppDatabase.kt
@@ -1,1 +1,28 @@
+package com.example.whitelistcheck4
 
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+
+@Database(entities = [HistoryEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun historyDao(): HistoryDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "whitelist_history"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
