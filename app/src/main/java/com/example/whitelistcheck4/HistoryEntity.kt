f9413e527f22fe0e181d3071acc1876b454acb3b
Us3rname5 <khalikovshamil1@gmail.com>
2026-07-23 12:58:21 +0300

    Update HistoryEntity.kt

--- app/src/main/java/com/example/whitelistcheck4/HistoryEntity.kt
+++ app/src/main/java/com/example/whitelistcheck4/HistoryEntity.kt
@@ -1,1 +1,13 @@
+package com.example.whitelistcheck4
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "history")
+data class HistoryEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val timestamp: Long = System.currentTimeMillis(),
+    val isRestricted: Boolean,
+    val statusesJson: String,
+    val location: String?
+)
